bf3daf369c110a58b27762e6592a4368960d4dbd
rscunha <rsc.net@gmail.com>
2022-07-30 18:35:19 -0300

    add feature 2

--- hello_nodejs/app.js
+++ hello_nodejs/app.js
@@ -1,3 +1,4 @@
-console.log("Hello Word!\n");
+console.log("Hello Word 0!\n")
+console.log("Hello Word 1!\n");
 console.log("Hello Word 2!\n");
-console.log("Hello Word 3\n");
+console.log("Hello Word 3!\n");
